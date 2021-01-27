aef75cda23b00fc5c0123226d8ce5668d2207762
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-01-27 09:48:43 +0100

    Prepare end-to-end test case

--- src/Integration/Test/CudotosiIntegrationTestBase.cs
+++ src/Integration/Test/CudotosiIntegrationTestBase.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Extensions;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Tash;
 using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CudotosiTestResources = Aspenlaub.Net.GitHub.CSharp.Cudotosi.Test.Properties.Resources;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Cudotosi.Integration.Test {
     public class CudotosiIntegrationTestBase {
+        protected IFolder TestFolder;
         protected readonly IContainer Container;
 
         public CudotosiIntegrationTestBase() {
             Container = new ContainerBuilder().RegisterForCudotosiIntegrationTest().Build();
+            TestFolder = new Folder(Path.GetTempPath()).SubFolder(GetType().Name);
+            TestFolder.CreateIfNecessary();
+            CudotosiTestResources.SamplePicture_XL.Save(SamplePictureXlFileName());
         }
 
         protected async Task<CudotosiWindowUnderTest> CreateCudotosiWindowUnderTestAsync() {
@@ -21,6 +32,17 @@ namespace Aspenlaub.Net.GitHub.CSharp.Cudotosi.Integration.Test {
             };
             await sut.RemotelyProcessTaskListAsync(process, tasks);
             return sut;
+        }
+
+        private string SamplePictureXlFileName() {
+            return TestFolder.FullName + @"\" + nameof(CudotosiTestResources.SamplePicture_XL) + ".jpg";
+        }
+
+        public virtual void Cleanup() {
+            Assert.IsTrue(TestFolder.Exists());
+            var deleter = new FolderDeleter();
+            Assert.IsTrue(deleter.CanDeleteFolder(TestFolder));
+            deleter.DeleteFolder(TestFolder);
         }
     }
 }
--- src/Integration/Test/CudotosiWindowTest.cs
+++ src/Integration/Test/CudotosiWindowTest.cs
@@ -1,12 +1,29 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Aspenlaub.Net.GitHub.CSharp.Tash;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Cudotosi.Integration.Test {
     [TestClass]
     public class CudotosiWindowTest : CudotosiIntegrationTestBase {
+        [TestCleanup]
+        public override void Cleanup() {
+            base.Cleanup();
+        }
+
         [TestMethod]
         public async Task CanOpenCudotosi() {
-            using (await CreateCudotosiWindowUnderTestAsync()) { }
+            using (await CreateCudotosiWindowUnderTestAsync()) {}
+        }
+
+        [TestMethod]
+        public async Task CanCutOutAnAreaFromPicture() {
+            using (var sut = await CreateCudotosiWindowUnderTestAsync()) {
+                await sut.InitializeAsync();
+                var process = await sut.FindIdleProcessAsync();
+                var tasks = new List<ControllableProcessTask>();
+                await sut.RemotelyProcessTaskListAsync(process, tasks);
+            }
         }
     }
 }
